7095461a2d6530745fa06b4a2c4e9783691510fa
narnavg <narnavg@gmail.com>
2023-10-30 13:35:06 +0200

    auth

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,11 @@
 # appliactionnnnnnnnnnnnnnnnnnnnnnnnnnnn
-
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('login/', TokenObtainPairView.as_view()),
+
 ]
